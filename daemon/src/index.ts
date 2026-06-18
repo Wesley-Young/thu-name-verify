6b423a3496c1556f2cb12e25c2a03e3ecd45551b
Wei FeiYang <wesley.f.young@outlook.com>
2026-06-18 21:09:10 +0800

    Serve statically built pages

--- daemon/src/index.ts
+++ daemon/src/index.ts
@@ -1,4 +1,5 @@
 import { serve } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono';
 
 import fraqCtx from './bot';
@@ -8,9 +9,26 @@ const app = new Hono();
 
 app.route('/api/register', register);
 
+// Serve static files from the "dist" directory
+app.use(
+  '*',
+  serveStatic({
+    root: '../web/dist',
+  }),
+);
+
+// SPA fallback
+app.get(
+  '*',
+  serveStatic({
+    path: './index.html',
+    root: '../web/dist',
+  }),
+);
+
 serve({
   fetch: app.fetch,
-  port: 6000,
+  port: 8000,
 });
 
 fraqCtx.start();
